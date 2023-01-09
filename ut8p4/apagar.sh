79a274e5413cdc11efe3f3795ee31da2997b3be2
ivanaledon <aluale2979@ieselcaminas.org>
2023-01-09 12:18:49 +0100

    apagar.sh

---
+++ ut8p4/apagar.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+reiniciar () {
+	{
+	reb=$(($min*60))
+        sleep $reb
+        reboot	
+        } &
+}
+
+
+res=0
+
+if [ $# -eq 0 ];then
+	while [ $res -ne 3 ]
+	do
+		clear
+		echo "1. Reiniciar ahora"
+		echo "2. Reiniciar en n minutos"
+		echo "3. Salir"
+		echo "Introduzca opcion:"
+		read res
+		case $res in
+			1)
+				min=0
+				res=3
+				;;
+			2)
+				echo "Introduzca el numero de minutos"
+				read min
+				res=3
+				;;
+			3)
+				exit
+				;;
+			*)
+				echo "Opcion no valida"
+				;;
+		esac
+	done
+	reiniciar
+
+elif [ $# -eq 1 ];then
+	reb=$1
+	reiniciar
+fi
