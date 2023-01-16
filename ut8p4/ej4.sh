91e01ee8ff8add0c98147de9e6b70a9734ae9be2
ivanaledon <aluale2979@ieselcaminas.org>
2023-01-16 12:22:34 +0100

    ej 5 y correcciones

--- ut8p4/ej4.sh
+++ ut8p4/ej4.sh
@@ -3,3 +3,6 @@
 fech=$(date)
 
 echo $fech >> /var/log/reinicios.txt
+
+# crontab
+# @reboot /home/admin01/Escritorio/ejercicios_shellscript/ut8p4/ej4.sh
---
+++ ut8p4/ej5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fech=$(date)
+
+echo $fech >> /home/admin01/time.txt
+
+# crontab
+# 5 * * * * /home/admin01/Escritorio/ejercicios_shellscript/ut8p4/ej5.sh
+
